6d2d323b68dded39c9727739d9cdfda8d7b1656c
zilurrane <zilurrane@gmail.com>
2023-07-02 18:55:35 +0530

    Added Chat UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,28 @@ import { useChat } from 'ai/react'
 import { useId } from 'react'
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({ id: useId() })
+  const { messages, input: messageInput, handleInputChange: handleMessageInputChange, handleSubmit: handleMessageSubmit } = useChat({ id: useId() })
+  console.log(messages)
   return (
-    <div>
-      {messages.map(m => (
-        <div key={m.id}>
-          {m.role}: {m.content}
+    <div className='flex w-screen h-screen flex-row bg-gray-700 justify-center'>
+      <div className='flex flex-col w-1/2 h-screen bg-gray-800 shadow-xl'>
+        <div className='p-5 overflow-y-scroll mb-10'>
+          {messages.map(m => (
+            <div key={m.id} className={m.role === "user"? "bg-transparent p-2": "bg-gray-700 p-2"}>
+              {m.content}
+            </div>
+          ))}
         </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+        <form onSubmit={handleMessageSubmit} className="absolute bottom-0 flex justify-between w-1/2 shadow-md border border-slate-700">
+          <input
+            className="w-full text-black p-2 outline-none"
+            value={messageInput}
+            placeholder="Say something..."
+            onChange={handleMessageInputChange}
+          />
+          <button className="px-5" type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
